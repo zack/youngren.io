3dc15350e5bc871a36d166545e00ed067d95a177
Zack Youngren <zyoungren@constantcontact.com>
2014-07-15 19:17:21 -0400

    lots of work on wftda html and coffee. needs rafactor

--- public_html/javascript/wftda.js
+++ public_html/javascript/wftda.js
@@ -4,7 +4,7 @@
   start = (function(_this) {
     return function() {
       return $.ajax({
-        url: '/data/skater_data',
+        url: '/data/skater_data.json',
         async: false,
         success: function(data) {
           return generate_charts(data);
@@ -15,7 +15,99 @@
 
   generate_charts = (function(_this) {
     return function(data) {
-      return console.log(data);
+      var apprentices, chart, members;
+      members = data['Members']['Chars'];
+      apprentices = data['Apprentices']['Chars'];
+      members.unshift('Members');
+      apprentices.unshift('Apprentices');
+      chart = c3.generate({
+        bindto: '#chart-characters',
+        size: {
+          width: 900
+        },
+        data: {
+          columns: [members, apprentices],
+          type: 'bar'
+        },
+        bar: {
+          width: {
+            ratio: 1
+          }
+        },
+        tooltip: {
+          format: {
+            title: function(d) {
+              return d + ' Character(s)';
+            },
+            value: function(v) {
+              return v + '%';
+            }
+          }
+        },
+        axis: {
+          x: {
+            label: {
+              text: 'Number of Characters',
+              position: 'outer-center'
+            }
+          },
+          y: {
+            label: {
+              text: 'Percentage of Skaters',
+              position: 'outer-middle'
+            }
+          }
+        },
+        color: {
+          pattern: ['pink', '#DDD']
+        }
+      });
+      members = data['Members']['Syllables'];
+      apprentices = data['Apprentices']['Syllables'];
+      members.unshift('Members');
+      apprentices.unshift('Apprentices');
+      return chart = c3.generate({
+        bindto: '#chart-syllables',
+        size: {
+          width: 900
+        },
+        data: {
+          columns: [members, apprentices],
+          type: 'bar'
+        },
+        bar: {
+          width: {
+            ratio: 1
+          }
+        },
+        tooltip: {
+          format: {
+            title: function(d) {
+              return d + ' Syllable(s)';
+            },
+            value: function(v) {
+              return v + '%';
+            }
+          }
+        },
+        axis: {
+          x: {
+            label: {
+              text: 'Number of Syllables',
+              position: 'outer-center'
+            }
+          },
+          y: {
+            label: {
+              text: 'Percentage of Skaters',
+              position: 'outer-middle'
+            }
+          }
+        },
+        color: {
+          pattern: ['pink', '#DDD']
+        }
+      });
     };
   })(this);
 
--- src/coffee/wftda.coffee
+++ src/coffee/wftda.coffee
@@ -1,11 +1,100 @@
 start = () =>
   $.ajax({
-    url: '/data/skater_data',
+    url: '/data/skater_data.json',
     async: false,
     success: (data) => generate_charts(data)
   })
 
 generate_charts = (data) =>
-  console.log data
+
+  members = data['Members']['Chars']
+  apprentices = data['Apprentices']['Chars']
+  members.unshift('Members')
+  apprentices.unshift('Apprentices')
+
+  chart = c3.generate({
+    bindto: '#chart-characters',
+    size: {width: 900},
+    data: {
+      columns: [
+        members,
+        apprentices
+      ],
+      type: 'bar'
+    },
+    bar: {
+      width: {
+        ratio: 1
+      }
+    },
+    tooltip: {
+      format: {
+        title: (d) -> d + ' Character(s)',
+        value: (v) -> v + '%'
+      }
+    },
+    axis: {
+      x: {
+        label: {
+          text: 'Number of Characters',
+          position: 'outer-center'
+        }
+      },
+      y: {
+        label: {
+          text: 'Percentage of Skaters',
+          position: 'outer-middle'
+        }
+      }
+    },
+    color: {
+      pattern: ['pink', '#DDD']
+    }
+  })
+
+  members = data['Members']['Syllables']
+  apprentices = data['Apprentices']['Syllables']
+  members.unshift('Members')
+  apprentices.unshift('Apprentices')
+
+  chart = c3.generate({
+    bindto: '#chart-syllables',
+    size: {width: 900},
+    data: {
+      columns: [
+        members,
+        apprentices
+      ],
+      type: 'bar'
+    },
+    bar: {
+      width: {
+        ratio: 1
+      }
+    },
+    tooltip: {
+      format: {
+        title: (d) -> d + ' Syllable(s)',
+        value: (v) -> v + '%'
+      }
+    },
+    axis: {
+      x: {
+        label: {
+          text: 'Number of Syllables',
+          position: 'outer-center'
+        }
+      },
+      y: {
+        label: {
+          text: 'Percentage of Skaters',
+          position: 'outer-middle'
+        }
+      }
+    },
+    color: {
+      pattern: ['pink', '#DDD']
+    }
+  })
 
 start()
