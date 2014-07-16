30134ad577ea149d6e7449d3bc5c03cf43df568e
Zack Youngren <zyoungren@constantcontact.com>
2014-07-16 00:12:41 -0400

    lots of work on charts

--- public_html/javascript/wftda.js
+++ public_html/javascript/wftda.js
@@ -21,7 +21,7 @@
       chart = c3.generate({
         bindto: '#chart-characters',
         size: {
-          width: 900
+          width: 710
         },
         data: {
           columns: [members, apprentices],
@@ -61,10 +61,10 @@
       });
       members = ['Members League Skaters'].concat(data['Members']['Syllables']);
       apprentices = ['Apprentice League Skaters'].concat(data['Apprentices']['Syllables']);
-      return chart = c3.generate({
+      chart = c3.generate({
         bindto: '#chart-syllables',
         size: {
-          width: 900
+          width: 710
         },
         data: {
           columns: [members, apprentices],
@@ -105,6 +105,44 @@
               position: 'outer-middle'
             }
           }
+        }
+      });
+      members = data['Members']['Letters'];
+      apprentices = data['Apprentices']['Letters'];
+      console.log(members);
+      return chart = c3.generate({
+        bindto: '#chart-letters',
+        size: {
+          width: 710
+        },
+        data: {
+          columns: [['0 Letters - Members', 0].concat(members[0]), ['1 Letter - Members', 0].concat(members[1]), ['2 Letters - Members', 0].concat(members[2]), ['3 Letters - Members', 0].concat(members[3]), ['0 Letters - Apprentices', 0].concat(apprentices[0]), ['1 Letter - Apprentices', 0].concat(apprentices[1]), ['2 Letters - Apprentices', 0].concat(apprentices[2]), ['3 Letters - Apprentices', 0].concat(apprentices[3])],
+          type: 'bar',
+          groups: [['0 Letters - Members', '1 Letter - Members', '2 Letters - Members', '3 Letters - Members'], ['0 Letters - Apprentices', '1 Letter - Apprentices', '2 Letters - Apprentices', '3 Letters - Apprentices']]
+        },
+        bar: {
+          width: {
+            ratio: 1
+          }
+        },
+        axis: {
+          x: {
+            label: {
+              text: 'Number of Characters',
+              position: 'outer-center'
+            },
+            min: 1,
+            max: 4
+          },
+          y: {
+            label: {
+              text: 'Percentage of Skaters',
+              position: 'outer-middle'
+            }
+          }
+        },
+        color: {
+          pattern: ['#1F77B4', '#4C99CF', '#84BFE8', '#C4E7FF', '#FF7F0E', '#FF9940', '#FFB573', '#FFD0A6']
         }
       });
     };
